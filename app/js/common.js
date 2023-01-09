defbb45e98041d3c22bcfa4399a7e68294805cfb
jukachu <Phenom_loh123>
2023-01-09 20:32:47 +0200

    note

--- app/js/common.js
+++ app/js/common.js
@@ -1,162 +1,41 @@
-//mouse hidden bg
-let bgHidden = document.querySelector('.hidden-bg .img');
+//hover stands
 
-function ifHiddenBg() {
-    if (bgHidden) {
-        function update(e) {
-            let rect = e.target.getBoundingClientRect();
-            var x = e.clientX - rect.left || e.touches[0].clientX - rect.left;
-            var y = e.clientY - rect.top || e.touches[0].clientY - rect.top;
+let shelfs = [...document.querySelectorAll('.plates-center ol li')];
+let mirrors = [...document.querySelectorAll('.mirror .img')];
 
-            bgHidden.style.setProperty('--cursorX', x + 'px');
-            bgHidden.style.setProperty('--cursorY', y + 'px');
-        }
+let partsComp = [...document.querySelectorAll('.part-back')];
+function shelfInteract() {
+    if (shelfs.length) {
+        let numberOfpart = 0;
+        shelfs.forEach((sh, k) => {
+            sh.addEventListener('mouseover', () => {
+                mirrors[k].classList.add('act');
+            });
+            sh.addEventListener('mouseout', () => {
+                mirrors[k].classList.remove('act');
+            });
+            sh.addEventListener('click', () => {
+                if (numberOfpart === 0) {
+                    partsComp[2].classList.add('hide');
 
-        bgHidden.addEventListener('mousemove', update)
-        bgHidden.addEventListener('touchmove', update);
-        bgHidden.addEventListener('mouseout', () => {
-            bgHidden.style.setProperty('--cursorX', '-50vw');
-            bgHidden.style.setProperty('--cursorY', '-50vh');
-        })
-    }
-}
+                }
+                if (numberOfpart === 1) {
+                    partsComp[0].classList.add('hide');
+                }
 
-ifHiddenBg();
+                if (numberOfpart === 2) {
+                    partsComp[1].classList.add('hide');
+                    document.querySelector('.backblock').classList.add('hide');
 
-
-
-//header scroll
-
-$(window).scroll(function (e) {
-    $el = $('.header');
-    $el.toggleClass('header-fixed', $(this).scrollTop() > 52);
-});
-
-function headerStart() {
-    let headr = document.querySelector('.header');
-    let hei = headr.offsetHeight;
-    // console.log(hei);
-
-}
-
-headerStart();
-
-//header scroll
-
-
-//odometer
-let trueLessInterval = [false, false, false];
-
-function isElementInViewport(el, k) {
-
-    // Special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-        el = el[0];
-    }
-
-    var rect = el.getBoundingClientRect();
-
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
-    );
-}
-
-function counter(id, start, end, duration, k) {
-    let obj = document.getElementById(`${id}`),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration / range)),
-        timer = setInterval(() => {
-            current += increment;
-            obj.innerHTML = current;
-            if (current == end) {
-                clearInterval(timer);
-                trueLessInterval[k] = true;
-            }
-        }, step);
-}
-
-
-$(window).scroll(function () {
-
-    let elem = document.querySelectorAll('.odometer');
-
-    elem.forEach((el, k) => {
-        if (trueLessInterval[k] === false) {
-            let data = el.dataset.val;
-
-            if (isElementInViewport(el, k)) {
-                trueLessInterval[k] = 3;
-                counter(el.id, 0, data, 1500, k);
-            }
-        }
-    });
-
-
-});
-
-//odometer
-
-//go next click
-
-let btnNext = [...document.querySelectorAll('.go-down')];
-
-function clickNext() {
-    if (btnNext.length) {
-        btnNext.forEach((btn) => {
-
-
-            btn.addEventListener('click', () => {
-                $([document.documentElement, document.body]).animate({
-                    scrollTop: $('.about-us').offset().top - $('.header').height()
-                }, 400);
+                }
+                numberOfpart++;
             })
-        })
+        });
     }
 }
+shelfInteract();
 
-clickNext();
-
-let herDown = [...document.querySelectorAll('.her-down')];
-
-function clickHer() {
-    if (herDown.length) {
-        herDown.forEach((btn) => {
-
-
-            btn.addEventListener('click', () => {
-                $([document.documentElement, document.body]).animate({
-                    scrollTop: $('.our-knowledge').offset().top - $('.header').height()
-                }, 400);
-            })
-        })
-    }
-}
-
-clickHer();
-
-//go next click
-
-let btnMobMoreText = [...document.querySelectorAll('.about-us__cont .text')];
-
-function openMobTextMore() {
-    if (btnMobMoreText.length) {
-        btnMobMoreText.forEach((btn) => {
-            let a = btn.querySelector('a');
-
-            if (window.innerWidth < 768) {
-                a.addEventListener('click', () => {
-
-                })
-            }
-        })
-    }
-}
-// openMobTextMore();
+//hover stands
 
 var allLazyLoad = [...document.querySelectorAll('.lazyload')];
 
@@ -176,112 +55,4 @@ function allLozadImg() {
 allLozadImg();
 
 
-//mouse follows
 
-
-// var backdrop = document.querySelector('.backdrop');
-
-let burger = [...document.querySelectorAll('.burger')];
-let centerMenu = [...document.querySelectorAll('.header-menu')][0];
-
-function burgerControl() {
-    if (burger.length) {
-        burger.forEach((btn) => {
-
-            btn.addEventListener('click', () => {
-                btn.classList.toggle('active');
-                centerMenu.classList.toggle('out');
-                // backdrop.classList.toggle('active');
-                document.body.classList.toggle('no-scroll')
-
-            })
-        })
-    }
-}
-
-burgerControl();
-
-
-//swipers
-
-let clientsSlider = [...document.querySelectorAll('.our-clients__wrap')];
-
-function clientsStartSlider() {
-    if (!clientsSlider.length) {
-
-    } else {
-        clientsSlider.forEach((sld) => {
-            let sldCont = sld.querySelector('.our-clients__slider');
-            let sldNext = sld.querySelector('.slider-btn--next');
-            let sldPrev = sld.querySelector('.slider-btn--prev');
-
-            const swiper2 = new Swiper(sldCont, {
-                // Optional parameters
-                loop: false,
-                slidesPerView: 1,
-                slidesPerGroup: 1,
-                speed: 600,
-                navigation: {
-                    nextEl: sldNext,
-                    prevEl: sldPrev,
-                },
-                autoplay: {
-                    delay: 3000,
-                },
-                spaceBetween: 6,
-                breakpoints: {
-
-                    767: {
-                        slidesPerView: 5,
-                        spaceBetween: 40,
-                        autoplay: false,
-                    }
-                }
-
-            });
-        })
-    }
-}
-
-clientsStartSlider();
-
-//swipers
-
-
-//single infos
-
-let infos = [...document.querySelectorAll('.our-infos__single')];
-
-function infosControl() {
-    if (infos.length) {
-        infos.forEach((inf, k) => {
-            let btn = inf.querySelector('.btn-infos');
-            btn.addEventListener('click', () => {
-                infos.forEach((inf2) => {
-                    inf2.classList.remove('open');
-                });
-                inf.classList.add('open');
-            })
-        })
-    }
-}
-
-
-
-infosControl();
-//single infos
-
-let btnTxt = [...document.querySelectorAll('.btn-txt')];
-
-function openTxt() {
-    if (btnTxt.length) {
-        btnTxt.forEach((btn) => {
-            btn.addEventListener('click', (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                btn.closest('.txt-block').classList.toggle('open');
-            })
-        })
-    }
-}
-openTxt();
